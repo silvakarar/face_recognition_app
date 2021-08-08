0af0f3cfc4a8f0e7f2af13a810afa888c2362f85
Silva Karar <markappleyard@silvas-iMac.local>
2021-08-08 12:54:21 -0700

    Added margin to text

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -4,7 +4,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className="f3 white center ">
+      <p className="f3 white center ml4 mr4">
         {'This brain will detect faces in your pictures. Give it a try.'}
       </p>
       <div className="center">
